0621c1b321881488fa3377871da0964d1fc48d6b
aimane yousr <aimaneyousr@gmail.com>
2023-02-21 11:47:23 +0100

    models & crud lecture form done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,3 @@
 class Restaurant < ApplicationRecord
+  validates :name, presence: true # now we definitely need a name for a restaurant to be created"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants
+  # get '/restaurants', to: 'restaurants#index'
+
+  # get '/restaurants/new', to: 'restaurants#new'
+  # post '/restaurants', to: 'restaurants#create'
+
+  # get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # get '/restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  # patch '/restaurants/:id', to: 'restaurants#update'
+  # delete '/restaurants/:id', to: 'restaurants#destroy'
 end
